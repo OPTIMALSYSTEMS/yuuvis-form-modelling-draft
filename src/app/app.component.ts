c293c6ffee2aeabd83c5213d2efac4edffe67a4a
Andreas Schulz <schulz@optimal-systems.de>
2023-09-01 16:07:49 +0200

    Basic setup and schema definitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import * as requestSchema from '../assets/schema.json';
+import * as testModel from '../assets/test-model.json';
+import Ajv from 'ajv';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'yuuvis-form-model-validator';
+
+  validationError?: any;
+
+  constructor(protected ajv: Ajv) {
+    this.validate(requestSchema, testModel);
+  }
+
+  public validate(schema: any, data: any) {
+    let isValid = false;
+    try {
+      isValid = this.ajv.validate(schema, data);
+    }
+    catch(e) {
+console.log(e);
+
+    }
+    if (!isValid) {
+      const errorMessages = this.ajv.errorsText();
+      this.validationError = JSON.stringify(this.ajv.errors, null, 2);
+      console.log(`Validation Error. ${errorMessages}`);
+      // throw new Error(`Validation Error. ${errorMessages}`);
+    }
+    return data;
+  }
 }
